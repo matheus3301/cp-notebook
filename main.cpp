12e73a3e2219b633c2eec13385d4c34e37c7b655
Matheus Rocha <monteiromatheus.rocha@gmail.com>
2020-07-07 17:48:26 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define debug(x) cerr << #x << " = " << x << endl;
+#define endl "\n"
+#define PB push_back
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+
+    return 0;
+}
